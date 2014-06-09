8ac765706b771fe91c73dc8fa60b08ef957ba1a4
Northnroro <northnroro@gmail.com>
2014-06-09 13:57:28 +0700

    Recruiter put Application in basket

--- app/views/recruiter/requisition/shortlist/candidate/ckbox.php
+++ app/views/recruiter/requisition/shortlist/candidate/ckbox.php
@@ -1,3 +1,17 @@
 <?php
-echo "Hello".Input::get('id');
+	$id = Input::get('id');
+	$application = Application::find($id);
 ?>
+
+<body style="background-color:transparent; ">
+	<form id='f' method='GET' action=<?php echo '../public/recruiter-shortlist-candidate-ckbox-ctrl/'.$id; ?>>
+		<input type='checkbox' <?php echo $application->is_in_basket?'checked':''; ?> onchange='toggleCandidate(this)' />
+	</form>
+</body>
+
+<script>
+	function toggleCandidate(x){
+		document.getElementById('f').style.backgroundColor = 'yellow';
+		document.getElementById('f').submit();
+	}
+</script>
