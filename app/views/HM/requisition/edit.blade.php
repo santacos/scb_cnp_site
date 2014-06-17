e4984cba3519e38b61f2595e2fcce2da9307bf66
santacos <santacos_lala@hotmail.com>
2014-06-17 15:56:12 +0700

    แก้แก้แก้แก้ คิคิคิ

--- app/views/HM/requisition/edit.blade.php
+++ app/views/HM/requisition/edit.blade.php
@@ -97,10 +97,13 @@ HM-create-requisition
 
                 <div class="form-group">
                     <label for="division">Division :</label>
-                    <select ng-model="requisition.division" ng-change="checkDivision()" class="form-control scrollable-menu" id="division" name="division">
-                         <option ng-repeat="position in allPosition | filter:{group:requisition.group} | unique:'division'" value="@{{position.division}}">@{{position.division}}</option>
+                    
+                    <select ng-model="requisition.division"  ng-change="checkDivision()"  
+                        class="form-control scrollable-menu ng-valid ng-dirty " 
+                        id="division" name="division"
+                        ng-options="position.division as position.division for position in allPosition  | filter:{group:requisition.group}    | unique:'division'">
                         
-                    </select>
+                    </select> 
                 </div>
 
                 <div class="form-group">
