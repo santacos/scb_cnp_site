70787b97d955beb2d8cd62703654fe7f75875683
santacos <santacos_lala@hotmail.com>
2014-06-19 14:07:28 +0700

    create with templateeeeeeee

--- public/js/createReq-manager.js
+++ public/js/createReq-manager.js
@@ -67,9 +67,12 @@ var nameApp = angular.module('nameApp',['ui.bootstrap','ui.utils','ngSanitize'])
 	  		$scope.count = 0;
 	  		$scope.isShowSkill=false;
 
+	  		//for template//
+	  		$scope.showtempqual=false;
 	  		$scope.showtemp=false;
 	  		$scope.text='yeah ckck';
 	  		$scope.showtext='Select option';
+	  		$scope.showqualtext='Select option';
 	  		$scope.setText=function(responText){
 	  			$scope.showtext=responText;
 	  			
@@ -89,8 +92,26 @@ var nameApp = angular.module('nameApp',['ui.bootstrap','ui.utils','ngSanitize'])
 	  		}
 	  		];
 
+	  		$scope.templateQual=[
+	  		{name:'option1',text:
+	  			'<ul><li>Overseeing and monitoring the internal rules, regulations, notification concerned the business&nbsp;issued by the Bank of Thailand.&nbsp;</li><li>Preparing compliance report focus on operation, advisory function to senior&nbsp;management on compliance law, rules and standards, including update on changes&nbsp;and developments in this area.</li><li>Creating business operation about action plans program and activities</li><li>Reviewing and monitoring internal compliance activities</li><li>Developing internal process related to the regulation and legal.</li></ul>'
+	  		},{name:'option2',text:
+	  		'<ul><li>Drive, develop and control the professional and competencies development process by&nbsp;:Contribute to the development of a good work environment with managers, employees and staff representatives by identifying, analyzing and addressing risks and establishing, fostering constructive dialogue.<ul><li>identifying with the managers the short and long term competency&nbsp;<br />requirements&nbsp;</li><li>offering and implementing a relevant (in terms of cost, quality and time)&nbsp;<br />training plan&nbsp;so as to meet the entity&rsquo;s objectives</li></ul></li><li>Contribute to the development of a good work environment with managers, employees, and staff representatives by identifying, analyzing and addressing risk and establishing fostering constructive dialogue</li><li>Organize the HR department and follow up the HR and training budget&nbsp;</li></ul>'
 
-	  		
+	  		},{name:'option3',text:
+	  			'hey3'
+	  		},{name:'option4',text:
+	  			'hey4'
+	  		},{name:'option5',text:
+	  			'hey5'
+	  		}
+	  		];
+
+	  		$scope.setShowqual=function(qualText){
+	  			$scope.showqualtext=qualText;
+	  			
+	  		};
+
 
 	  		//skill relate
 	  		$scope.skills=[
@@ -257,6 +278,53 @@ var nameApp = angular.module('nameApp',['ui.bootstrap','ui.utils','ngSanitize'])
 			 	$scope.count = $scope.temp;
 
 			 };
+
+			 $scope.resetqual=function(){
+			 	$scope.qualification='';
+			 	$scope.temp=0;
+			 	if($scope.group){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.division){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.organization){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.job_title){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.corporate_title_id){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.totalNumber>0){
+			 		$scope.temp = $scope.temp+1;
+			 		console.log($scope.totalNumber);
+			 	}
+			 	if($scope.recruitment_obj){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.recruitment_type){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.location_id){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.year_of_experience){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.responsibility){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+			 	if($scope.qualification){
+			 		$scope.temp = $scope.temp+1;
+			 	}
+
+			 	$scope.count = $scope.temp;
+
+			 }
+
+
 
 			 $scope.addSkill = function(){
 			 	$scope.skills.push({category:$scope.tempBranch,name:$scope.tempName});
