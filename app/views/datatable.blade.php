06b15b22b390555a3071a5168215e349c4c5f00b
santacos <santacos_lala@hotmail.com>
2014-06-19 14:22:09 +0700

    dd

--- app/views/datatable.blade.php
+++ app/views/datatable.blade.php
@@ -1,3 +1,4 @@
+<div style="overflow:scroll;overflow-y:hidden;">
 <table  class="table table-hover table-bordered text-center {{ $class = str_random(8) }}" >
     <colgroup>
         @for ($i = 0; $i < count($columns); $i++)
@@ -60,6 +61,7 @@
 
     </tbody>
 </table>
+</div>
 <script type="text/javascript">
     
     jQuery(document).ready(function(){
