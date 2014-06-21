e4dc6277808a072ca02191d2162b87233b8d5002
peerapat <peerapat40595@gmail.com>
2014-06-21 13:56:18 +0700

    Search Job Status

--- app/controllers/CandidateController.php
+++ app/controllers/CandidateController.php
@@ -16,6 +16,34 @@ class CandidateController extends \BaseController {
 	{
 		return View::make('user.jobDetail')->with('requisition',Requisition::find($id));
 	}
+	public function getJobstatus($id)
+	{	
+		$search = trim(Input::get( 'search' ));
+        $status = trim(Input::get( 'status' ));
+		$apps=Application::where('candidate_user_id','=',$id);
+		if($status!=''&&$status!=0)
+		{	
+			$apps=$apps->where('application_current_status_id','=',$status);
+		}
+		
+		if($search!='')
+		{		
+			$searchTerms = explode(' ', $search);
+			 foreach($searchTerms as $term)
+			    {
+			    	 $GLOBALS['term'] = $term;
+			        $apps->whereHas('requisition', function($q) {
+				   		$q->where('job_title', 'LIKE', '%'.  $GLOBALS['term'] .'%');
+				     
+				     });
+			    }
+
+		}
+		$applications=$apps->paginate(10);
+	
+		return View::make('user.jobStatus', compact('applications'))->with(array('search'=>$search,'status'=>$status));	
+	}
+	
 	/**
 	 * Show the form for creating a new candidate
 	 *
