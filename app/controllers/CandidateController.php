f2fde7136de1e3210ecbfbc502388349afae1920
peerapat <peerapat40595@gmail.com>
2014-06-24 15:37:45 +0700

    Update

--- app/controllers/CandidateController.php
+++ app/controllers/CandidateController.php
@@ -1,3 +1,128 @@
+<?php 
+class DocxConversion{
+    private $filename;
+
+    public function __construct($filePath) {
+        $this->filename = $filePath;
+    }
+
+    private function read_doc() {
+        $fileHandle = fopen($this->filename, "r");
+        $line = @fread($fileHandle, filesize($this->filename));   
+        $lines = explode(chr(0x0D),$line);
+        $outtext = "";
+        foreach($lines as $thisline)
+          {
+            $pos = strpos($thisline, chr(0x00));
+            if (($pos !== FALSE)||(strlen($thisline)==0))
+              {
+              } else {
+                $outtext .= $thisline." ";
+              }
+          }
+         $outtext = preg_replace("/[^a-zA-Z0-9\s\,\.\-\n\r\t@\/\_\(\)]/","",$outtext);
+        return $outtext;
+    }
+
+    private function read_docx(){
+
+        $striped_content = '';
+        $content = '';
+
+        $zip = zip_open($this->filename);
+
+        if (!$zip || is_numeric($zip)) return false;
+
+        while ($zip_entry = zip_read($zip)) {
+
+            if (zip_entry_open($zip, $zip_entry) == FALSE) continue;
+
+            if (zip_entry_name($zip_entry) != "word/document.xml") continue;
+
+            $content .= zip_entry_read($zip_entry, zip_entry_filesize($zip_entry));
+
+            zip_entry_close($zip_entry);
+        }// end while
+
+        zip_close($zip);
+
+        $content = str_replace('</w:r></w:p></w:tc><w:tc>', " ", $content);
+        $content = str_replace('</w:r></w:p>', "\r\n", $content);
+        $striped_content = strip_tags($content);
+
+        return $striped_content;
+    }
+
+ /************************excel sheet************************************/
+
+function xlsx_to_text($input_file){
+    $xml_filename = "xl/sharedStrings.xml"; //content file name
+    $zip_handle = new ZipArchive;
+    $output_text = "";
+    if(true === $zip_handle->open($input_file)){
+        if(($xml_index = $zip_handle->locateName($xml_filename)) !== false){
+            $xml_datas = $zip_handle->getFromIndex($xml_index);
+            $xml_handle = DOMDocument::loadXML($xml_datas, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+            $output_text = strip_tags($xml_handle->saveXML());
+        }else{
+            $output_text .="";
+        }
+        $zip_handle->close();
+    }else{
+    $output_text .="";
+    }
+    return $output_text;
+}
+
+/*************************power point files*****************************/
+function pptx_to_text($input_file){
+    $zip_handle = new ZipArchive;
+    $output_text = "";
+    if(true === $zip_handle->open($input_file)){
+        $slide_number = 1; //loop through slide files
+        while(($xml_index = $zip_handle->locateName("ppt/slides/slide".$slide_number.".xml")) !== false){
+            $xml_datas = $zip_handle->getFromIndex($xml_index);
+            $xml_handle = DOMDocument::loadXML($xml_datas, LIBXML_NOENT | LIBXML_XINCLUDE | LIBXML_NOERROR | LIBXML_NOWARNING);
+            $output_text .= strip_tags($xml_handle->saveXML());
+            $slide_number++;
+        }
+        if($slide_number == 1){
+            $output_text .="";
+        }
+        $zip_handle->close();
+    }else{
+    $output_text .="";
+    }
+    return $output_text;
+}
+
+
+    public function convertToText() {
+
+        if(isset($this->filename) && !file_exists($this->filename)) {
+            return "File Not exists";
+        }
+
+        $fileArray = pathinfo($this->filename);
+        $file_ext  = $fileArray['extension'];
+        if($file_ext == "doc" || $file_ext == "docx" || $file_ext == "xlsx" || $file_ext == "pptx")
+        {
+            if($file_ext == "doc") {
+                return $this->read_doc();
+            } elseif($file_ext == "docx") {
+                return $this->read_docx();
+            } elseif($file_ext == "xlsx") {
+                return $this->xlsx_to_text();
+            }elseif($file_ext == "pptx") {
+                return $this->pptx_to_text();
+            }
+        } else {
+            return "Invalid File Type";
+        }
+    }
+
+}
+?>
 <?php
 
 class CandidateController extends \BaseController {
@@ -161,13 +286,16 @@ class CandidateController extends \BaseController {
 				$cv = Input::file('filepath_cv');
 				$filename = date('Y-m-d-H-i-s')."-".$cv->getClientOriginalName();
 				$uploadSuccess   = $cv->move(public_path().'/cv/candidatecvs/', $filename);
+				$docObj = new DocxConversion(public_path().'/cv/candidatecvs/'.$filename); 
+				//$docObj = new Doc2Txt("test.doc"); 
+				$txt = $docObj->convertToText(); 
+				// return $txt;
 				// Image::make($image->getRealPath())->save(public_path().'/cv/candidatecvs/'.$filename);
 				File::delete(public_path().$candidate->filepath_cv);
 				$candidate->filepath_cv = '/cv/candidatecvs/'.$filename;
 
-			}else if (Input::has('filepath_cv')) {
-				File::delete(public_path().$candidate->filepath_cv);
-				$candidate->filepath_cv = Input::get('filepath_cv');
+
+
 			}
 			if(Input::hasFile('filepath_video')){
 				$video = Input::file('filepath_video');
