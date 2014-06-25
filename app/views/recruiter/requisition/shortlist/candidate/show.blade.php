df41ef6e666e749bb6e35c1705eeb3086041b485
santacos <santacos_lala@hotmail.com>
2014-06-25 22:44:07 +0700

    search select candidate ตรง quick search ไม่ได้เชื่อมกะ back อ่ะ อยากได้ช่อง search จาก datatable

--- app/views/recruiter/requisition/shortlist/candidate/show.blade.php
+++ app/views/recruiter/requisition/shortlist/candidate/show.blade.php
@@ -67,7 +67,20 @@ app.controller('appCtrl',['$scope', '$http',
             <div class="tab-pane active" id="quick">
               <div class="panel panel-default">
                 <div class="panel-body">
-                  Quick search
+                  {{ Form::label('keyword','Keyword : ')}}
+                  <br>
+                  <input type="text" class="form-control" placeholder="Enter ...">
+                  <hr>
+                  <div class="row">
+                    <div class="col col-md-4">
+                    </div>
+                    
+                    <div class="col col-md-3">
+                      <button class="btn btn-warning btn-lg" style="width:100%;" type="submit">Submit</button>
+                    </div>
+                    <div class="col col-md-4">
+                    </div>
+                  </div><!--end second row-->
                 </div>
               </div>
             </div>
@@ -76,7 +89,7 @@ app.controller('appCtrl',['$scope', '$http',
                 <div class="panel-body">
                   {{Form::open(array('url' => 'recruiter-shortlist-candidate/'.$requisition_id, 'method' => 'get'))}}
                   <div class="panel-group" id="education">
-                     <div class="panel panel-primary">
+                     <div class="panel panel-info">
                       <div class="panel-heading">
                         <h4 class="panel-title">
                           <a data-toggle="collapse" data-parent="#education" href="#eduSearch">
@@ -85,31 +98,180 @@ app.controller('appCtrl',['$scope', '$http',
                         </h4>
                       </div>
                       <div id="eduSearch" class="panel-collapse collapse">
-                        <div class="panel-body">
-                          
+                        <div class="panel-body" style="padding-bottom:2em;">
+                          <div class="row">
+                            <div class="col col-md-10 col-md-offset-1">
+
+                              <div class="panel panel-default">
+                                <div class="panel-body">
+                                  <strong>Degree :</strong><br>
+                                    <div class="row">
+                                      <div class="col col-md-2"></div>
+                                      <div class="col col-md-2">
+                                        {{Form::checkbox('bachelor', 1,isset($input['bachelor'])?$input['bachelor']:'');}}
+                                        {{ Form::label('bachelor','Bachelor')}}
+                                      </div>
+                                      <div class="col col-md-2">
+                                        {{Form::checkbox('master', 1,isset($input['master'])?$input['master']:'');}}
+                                        {{ Form::label('master','Master')}}
+                                      </div>
+                                      <div class="col col-md-2">
+                                        {{Form::checkbox('doctor', 1,isset($input['doctor'])?$input['doctor']:'');}}
+                                        {{ Form::label('doctor','Doctor')}}
+                                      </div>
+                                      
+                                    </div>
+                                </div>
+                              </div>
+                              <hr>
+                              {{ Form::label('field_of_study','Field of study : ')}}
+                              <br>
+                              {{ Form::text('field_of_study',  isset($input['field_of_study'])?$input['field_of_study']:'', array('class' => 'form-control','placeholder'=>'search field of study','style'=>'font-size:1.1em;')) }}
+
+                              <br>
+                              {{ Form::label('major','Major : ')}}
+                              <br>
+                              {{ Form::text('major', isset($input['major'])?$input['major']:'', array('class' => 'form-control','placeholder'=>'search major','style'=>'font-size:1.1em;')) }}
+                              <br>
+                              {{ Form::label('school_name','Name of institution : ')}}
+                              <br>
+                              {{ Form::text('school_name', isset($input['school_name'])?$input['school_name']:'', array('class' => 'form-control','placeholder'=>'search name of institution','style'=>'font-size:1.1em;')) }}
+                           
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
 
-                  <div class="panel-group" id="accordion2">
-                    <div class="panel panel-info">
+                  <div class="panel-group" id="experience">
+                    <div class="panel panel-default">
                       <div class="panel-heading">
                         <h4 class="panel-title">
-                          <a data-toggle="collapse" data-parent="#accordion2" href="#collapseOne2">
-                            Collapsible Group Item #1
+                          <a data-toggle="collapse" data-parent="#experience" href="#expSearch">
+                            Work experience
                           </a>
                         </h4>
                       </div>
-                      <div id="collapseOne2" class="panel-collapse collapse in">
+                      <div id="expSearch" class="panel-collapse collapse">
                         <div class="panel-body">
-                          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                          <div class="row">
+                            <div class="col col-md-offset-1 col-md-10">
+                              {{ Form::label('company_name','Company name : ')}}
+                              <br>
+                              {{ Form::text('company_name', isset($input['company_name'])?$input['company_name']:'', array('class' => 'form-control','placeholder'=>'search company name','style'=>'font-size:1.1em;')) }}
+                              <br>
+                              {{ Form::label('location','Location : ')}}
+                              <br>
+                              {{ Form::text('location', isset($input['location'])?$input['location']:'', array('class' => 'form-control','placeholder'=>'search location of company','style'=>'font-size:1.1em;')) }}
+
+                              <br>
+                              {{ Form::label('position','Position : ')}}
+                              <br>
+                              {{ Form::text('position', isset($input['position'])?$input['position']:'', array('class' => 'form-control','placeholder'=>'search position','style'=>'font-size:1.1em;')) }}
+
+                              <br>
+                              {{Former::select('monthly_salary','Monthly Salary :')->addOption('Select Range')->options(array('>=','<=','between'))->attributes(array('ng-model'=>'monthly_salary'))}}
+                              
+                              <input ng-show="monthly_salary==='0'||monthly_salary==1||monthly_salary==2"name="monthly_salary1" type="number" min=0 max=1000000 class="form-control" value="{{isset($input['monthly_salary1'])?$input['monthly_salary1']:''}}">
+                              <input ng-show="monthly_salary==2"name="monthly_salary2" type="number" min=0 max=1000000 class="form-control" value="{{isset($input['monthly_salary2'])?$input['monthly_salary2']:''}}">
+                              <br>  
+                              {{Former::select('experience','Year of experience :')->addOption('Select Range')->options(array('>=','<=','between'))->attributes(array('ng-model'=>'experience'))}}
+                              <input ng-show="experience==='0'||experience==1||experience==2"name="experience1" type="number" min=0 max=50 class="form-control" value="{{isset($input['experience1'])?$input['experience1']:''}}">
+                              <input ng-show="experience==2"name="experience2" type="number" min=0 max=50 class="form-control" value="{{isset($input['experience2'])?$input['experience2']:''}}">
+                              <br>
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
                   </div>
 
-                  
+                  <div class="panel-group" id="skill">
+                     <div class="panel panel-info">
+                      <div class="panel-heading">
+                        <h4 class="panel-title">
+                          <a data-toggle="collapse" data-parent="#skill" href="#skillSearch">
+                            Skill
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="skillSearch" class="panel-collapse collapse">
+                        <div class="panel-body" style="padding-bottom:2em;">
+                          <div class="row">
+                            <div class="col col-md-10 col-md-offset-1">
+                              {{ Form::label('skill','Skill : ')}}
+                              <br>
+                              {{ Form::text('skill', isset($input['skill'])?$input['skill']:'', array('class' => 'form-control','placeholder'=>'search skill','style'=>'font-size:1.1em;')) }}
+                              <br>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="panel-group" id="score">
+                     <div class="panel panel-default">
+                      <div class="panel-heading">
+                        <h4 class="panel-title">
+                          <a data-toggle="collapse" data-parent="#score" href="#scoreSearch">
+                            Score from prescreening question
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="scoreSearch" class="panel-collapse collapse">
+                        <div class="panel-body" style="padding-bottom:2em;">
+                          <div class="row">
+                            <div class="col col-md-10 col-md-offset-1">
+                              {{Former::select('score','Score :')->addOption('Select Range')->options(array('>=','<=','between'))->attributes(array('ng-model'=>'score'))}}
+                              <input ng-show="score==='0'||score==1||score==2"name="score1" type="number" min=0 max=1000 class="form-control" value="{{isset($input['score1'])?$input['score1']:''}}">
+                              <input ng-show="score==2"name="score2" type="number" min=0 max=1000 class="form-control" value="{{isset($input['score2'])?$input['score2']:''}}">
+                              <br>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div class="panel-group" id="resume">
+                    <div class="panel panel-info">
+                      <div class="panel-heading">
+                        <h4 class="panel-title">
+                          <a data-toggle="collapse" data-parent="#resume" href="#resumeSearch">
+                            Resume search
+                          </a>
+                        </h4>
+                      </div>
+                      <div id="resumeSearch" class="panel-collapse collapse">
+                        <div class="panel-body">
+                          <div class="row">
+                            <div class="col col-md-offset-1 col-md-10">
+                              {{ Form::label('resume','Resume : ')}}
+                              <br>
+                              {{ Form::text('resume', isset($input['resume'])?$input['resume']:'', array('class' => 'form-control','placeholder'=>'search in resume','style'=>'font-size:1.1em;')) }}
+                              <br>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+
+                  <hr>
+                  <div class="row">
+                    <div class="col col-md-4">
+                    </div>
+                    
+                    <div class="col col-md-3">
+                      <button class="btn btn-primary btn-lg" style="width:100%;" type="submit">Submit</button>
+                    </div>
+                    <div class="col col-md-4">
+                    </div>
+                  </div><!--end second row-->
+
+                  {{Form::close()}}
+
 
                 </div><!--end panel-->
               </div><!--end panel default-->
@@ -123,7 +285,7 @@ app.controller('appCtrl',['$scope', '$http',
      <!-- first data table for requisition-->
      <div class="box box-solid box-primary">
       <!-- /.box-header -->
-      {{Form::open(array('url' => 'recruiter-shortlist-candidate/'.$requisition_id, 'method' => 'get'))}}
+    <!--   {{Form::open(array('url' => 'recruiter-shortlist-candidate/'.$requisition_id, 'method' => 'get'))}}
 
       {{ Form::label('Education')}}
       <br>
@@ -150,7 +312,7 @@ app.controller('appCtrl',['$scope', '$http',
       {{ Form::label('school_name','Name of institution : ')}}
       <br>
       {{ Form::text('school_name', isset($input['school_name'])?$input['school_name']:'', array('class' => 'form-control','placeholder'=>'search name of institution','style'=>'font-size:1.1em;')) }}
-      <!-- Experience -->
+     
       <br>
       {{ Form::label('Experience')}}
       <br>
@@ -177,20 +339,20 @@ app.controller('appCtrl',['$scope', '$http',
       <input ng-show="experience==='0'||experience==1||experience==2"name="experience1" type="number" min=0 max=50 class="form-control" value="{{isset($input['experience1'])?$input['experience1']:''}}">
       <input ng-show="experience==2"name="experience2" type="number" min=0 max=50 class="form-control" value="{{isset($input['experience2'])?$input['experience2']:''}}">
       <br>
-      <!-- Skill -->
+      
       <br>
       {{ Form::label('skill','Skill : ')}}
       <br>
       {{ Form::text('skill', isset($input['skill'])?$input['skill']:'', array('class' => 'form-control','placeholder'=>'search skill','style'=>'font-size:1.1em;')) }}
       <br>
-      <!-- Score -->
+      
 
       <br>
       {{Former::select('score','Score :')->addOption('Select Range')->options(array('>=','<=','between'))->attributes(array('ng-model'=>'score'))}}
       <input ng-show="score==='0'||score==1||score==2"name="score1" type="number" min=0 max=1000 class="form-control" value="{{isset($input['score1'])?$input['score1']:''}}">
       <input ng-show="score==2"name="score2" type="number" min=0 max=1000 class="form-control" value="{{isset($input['score2'])?$input['score2']:''}}">
       <br>
-      <!-- reseume -->
+      
       <br>
       {{ Form::label('resume','Resume : ')}}
       <br>
@@ -205,9 +367,9 @@ app.controller('appCtrl',['$scope', '$http',
         <div class="col col-md-4">
           <button class="btn btn-default " style="width:100%;" type="submit">Submit</button>
         </div>
-      </div><!--end second row-->
+      </div>
 
-      {{Form::close()}}
+      {{Form::close()}} -->
       <!--table style "table-striped"-->
       <div class="box-body table-responsive no-padding">
 
